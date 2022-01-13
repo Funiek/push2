bbd141d27df2c02128d43dfe392305ac3ba85f3a
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2022-01-13 19:08:11 +0100

    'fix0.2'

--- webpush.js
+++ webpush.js
@@ -1,25 +1,28 @@
-const webpush = require('web-push');
+const webpush = require("web-push");
+
+// let vapidKeys = webpush.generateVAPIDKeys();
+// console.log(vapidKeys);
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
   publicKey:
-    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
-  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+    "BBqRzfqF_x2LxB-23TfyXjrFjQvgVoYJikJ6RZ5TSQQjFGkwric_gRPJNdMoID0f0x_Ypg4NzziuzgZNk9v-HhY",
+  privateKey: "kYquKwcMajqi3XUPclaCZZYdUkIq-aRl4TQzAlnSrco",
 };
 
 webpush.setVapidDetails(
-  'mailto:example@yourdomain.org',
+  "mailto:funkowski.krzysztof@gmail.com",
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  endpoint: '.....',
+  endpoint: ".....",
   keys: {
-    auth: '.....',
-    p256dh: '.....'
-  }
+    auth: ".....",
+    p256dh: ".....",
+  },
 };
 
-webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
+webpush.sendNotification(pushSubscription, "Your Push Payload Text");
