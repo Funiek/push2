2d665f4bcfb62ab5e9f34f54bd8735b8f1475ed9
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2022-01-13 19:31:23 +0100

    'fix0.6'

--- webpush.js
+++ webpush.js
@@ -19,12 +19,12 @@ webpush.setVapidDetails(
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/cntV7aVST6I:APA91bGp8lN4IvWRwAGEAqS4CuWXl6a0caSbA4sG23TjYxFJBWXiOJhBczh56PR8M2jfROINz1Zbl1pyXw_ubymfUClv6k6wzwynrDoYwImmbVVYd1jCFnnG2sSScCvPjSsNxP-zpO5g",
+    "https://fcm.googleapis.com/fcm/send/csGxFMohTgE:APA91bGFUWT7Th7i207wIjVvte_BbTIk751UCXqR9SV137-jbLhNU6R5_Rahf3or6GXgsdoHKN-NazywT6fCYo5K6QmwIX4Fsg6lAQeAZpSA8t-Tf0SLWJWADOpQeytL7svid-vGv9Av",
   expirationTime: null,
   keys: {
     p256dh:
-      "BDcQ3RT3HuobJjgLpq3DXtVM9SuoLh_5PHEZexihqp0wYO3Xg031-9zB-DpiSKTjguEdiEHO3UPMxkYN9U62mPw",
-    auth: "npdkNuFyZvLhADw5nEFgDw",
+      "BIpHf1TlotzrF0G5vaim7A79QNm9vy0msGkQvl18FWvJuAlw9_OTYzGpONvjTEuF_PeZu1GXRSHZ6PqxYySVB1A",
+    auth: "oW6KH32ualGOrdMdrS0DvA",
   },
 };
 
