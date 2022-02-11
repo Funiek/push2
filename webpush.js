f8474303c8bd4f6853a2950fa3571e381dc39775
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2022-02-11 11:57:23 +0100

    oby poszlo

--- webpush.js
+++ webpush.js
@@ -3,11 +3,10 @@ const webpush = require("web-push");
 // let vapidKeys = webpush.generateVAPIDKeys();
 // console.log(vapidKeys);
 
-// VAPID keys should only be generated only once.
 const vapidKeys = {
   publicKey:
-    "BBqRzfqF_x2LxB-23TfyXjrFjQvgVoYJikJ6RZ5TSQQjFGkwric_gRPJNdMoID0f0x_Ypg4NzziuzgZNk9v-HhY",
-  privateKey: "kYquKwcMajqi3XUPclaCZZYdUkIq-aRl4TQzAlnSrco",
+    "BMGhTvZMnp2MIdajNZ6zsqTv7r6s27h3GhgFFYP4HvEuMLbBWkmPBbonNjXYK4ayv6gsOK3Jz3Y14jGCK9JsDlc",
+  privateKey: "2NrDZrqazu8LweNto8PGLn_TyKDL8hWGiG-9L_4GTm0",
 };
 
 webpush.setVapidDetails(
@@ -16,16 +15,15 @@ webpush.setVapidDetails(
   vapidKeys.privateKey
 );
 
-// This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/dE_tkILFD20:APA91bG_7Zd1hUmvDNh0O5jut7jYzkUvKCheD_nGbiHKCcbsODtvsTC71LalNCgCwYlGi1NGUoLlQBAqxd0xz1keWsc8dn2IIIxkOegdZ3KIPUrRtkQTbWnCQCPmteKHYRp6URhRgRVm",
+    "https://fcm.googleapis.com/fcm/send/fLcb8yqzoEQ:APA91bHkfW-Qxw-k9NkZ_1PnuYvNDNquN1N_ApfWMEl-LZZSHasF4FU8tTHc3P1om1v7WbHKvDiSUjeEhCir3ky-uG6l92Dp_yqOY0ZBaDiZsn7qMY4wEsFgzvGO2xTK53e9PwaZNeJp",
   expirationTime: null,
   keys: {
     p256dh:
-      "BOezgW7hkHevgrea2XECfpVgZWNnBOtXFYUFv5fWM_MoAK-yTE0kYx2gSMSrONSIZjEspdeyKOzIdth4FG7Yx_0",
-    auth: "-uOmNLpoLKSegEVL-NFweg",
+      "BNvROs8su3ZgX0MevHcPMwX-ET2w6hqkOttm8XD26dXt4D7ZwEfEoEOny812zex-INUnwWbhlbfLpZrRkNVsmEo",
+    auth: "qzYBXNPTJScgQDy-KLoPcw",
   },
 };
 
-webpush.sendNotification(pushSubscription, "Cycki Dupa");
+webpush.sendNotification(pushSubscription, "GAIA");
